01c8755c0e7260ba4d352c745b44fc6144a6078b
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2024-12-23 17:31:21 +0900

    Title: 규칙을 찾아 출력하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 241223/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -6,7 +6,7 @@ for i in range(1,n+1):
             print('*',end = '')
         print()
     else:
-        for j in range(i):
+        for j in range(i-1):
             print('*',end='')
         for j in range(n-i):
             print(' ',end='')
