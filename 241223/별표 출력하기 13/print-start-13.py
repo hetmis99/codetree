d6182911daabadb1a19a3fc3ed41e525735be43d
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2024-12-23 15:59:02 +0900

    Title: 별표 출력하기 13, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241223/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,23 @@
+n = int(input())
+string = []
+ele = ""
+
+for i in range(n, n//2, -1):
+    for j in range(i):
+        ele += "* "
+    string.append(ele)
+    ele = ""
+
+    if i == n//2+1: break
+    
+    for j in range(n-i+1):
+        ele += "* "
+    string.append(ele)
+    ele = ""
+
+rev = string[::-1]
+
+for i in string:
+    print(i)
+for i in rev:
+    print(i)
