84c8dfc555105764d486fc48378bb26fc694bc62
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2024-12-23 17:34:42 +0900

    Title: 규칙을 찾아 출력하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241223/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 241223/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -3,11 +3,11 @@ n =int(input())
 for i in range(1,n+1):
     if(i == 1 or i == n): 
         for j in range(n):
-            print('*',end = '')
+            print('* ',end = '')
         print()
     else:
         for j in range(i-1):
-            print('*',end='')
+            print('* ',end='')
         for j in range(n-i):
-            print(' ',end='')
+            print('  ',end='')
         print('*')
