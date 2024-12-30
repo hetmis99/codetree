a24ca9407da3695becb08ed6e4582462228f039c
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2024-12-31 05:18:39 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241231/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241231/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,30 +1,32 @@
-Number,ammo,first,T= map(int, input().split())
+Number, ammo, first, T = map(int, input().split())
 arr = [0 for i in range(Number)]
 left = [0 for i in range(Number)]
-arr[first-1], left[first-1] = 1,ammo
+arr[first - 1], left[first - 1] = 1, ammo
 state = []
 
-
 for i in range(T):
-    tick,x,y = map(int, input().split())
-    state.append((tick,x,y))
+    tick, x, y = map(int, input().split())
+    state.append((tick, x, y))
 
-#sort state, key = tick
+# sort state, key = tick
 state = sorted(state, key=lambda key: key[0])
 for i in range(len(state)):
-    tick,x,y = state[i]
-    if left[x-1] > 0:
-        left[x-1]-=1
-        if left[y-1]>0:
-            left[y-1]=ammo
-        arr[y-1]=1
-        
-    if left[y-1] > 0:
-        left[y-1]-=1
-        if left[x-1]>0:
-            left[x-1]=ammo
-        arr[x-1]=1
-    
+    tick, x, y = state[i]
+    if left[x - 1] > 0:
+        left[x - 1] -= 1
+        if arr[y - 1] == 0:
+            left[y - 1] = ammo
+        arr[y - 1] = 1
+
+    if left[y - 1] > 0:
+        left[y - 1] -= 1
+        if arr[x - 1] == 0:
+            left[x - 1] = ammo
+        arr[x - 1] = 1
+
+for i in state:
+    a,b,c = i
+    print(a,b,c)
 
 for i in arr:
     print(i, end='')
