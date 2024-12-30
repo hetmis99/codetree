2b39b493a899312a7bf6fb41e3be2bdb93d79c9c
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2024-12-31 05:03:15 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241231/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,25 @@
+Number,ammo,first,T= map(int, input().split())
+arr = [0 for i in range(Number)]
+left = [0 for i in range(Number)]
+arr[first-1], left[first-1] = 1,2
+state = []
+
+
+for i in range(T):
+    tick,x,y = map(int, input().split())
+    state.append((tick,x,y))
+
+#sort state, key = tick
+state = sorted(state, key=lambda key: key[0])
+for i in range(len(state)):
+    tick,x,y = state[i]
+    if left[x-1] > 0:
+        left[x-1]-=1
+        arr[y-1]=1
+    if left[y-1] > 0:
+        left[y-1]-=1
+        arr[x-1]=1
+    
+
+for i in arr:
+    print(i, end='')
