9df444e930e5c4c1f22677326c91894597a7c764
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2024-12-31 00:52:32 +0900

    Title: 신기한 타일 뒤집기, Time: 105ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 241231/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,30 @@
+n = int(input())
+# Initialize array with tuples (count, direction)
+arr = [(0, 0, "") for _ in range(200002)]
+cur = 100000
+
+for _ in range(n):
+    inp = input().split()
+    amount = int(inp[0])
+    direction = inp[1]
+    
+    if direction == 'R':
+        for j in range(cur, cur + amount):
+            rcount, lcount, dir = arr[j]
+            arr[j] = (rcount + 1, lcount, 'R')
+    else:  # direction == 'L'
+        for j in range(cur, cur - amount, -1):
+            rcount, lcount, dir = arr[j]
+            arr[j] = (rcount, lcount + 1, 'L')
+    
+    if direction == 'R': cur += amount - 1 
+    else: cur -= amount - 1
+
+w, b = 0, 0
+for rcount, lcount, dir in arr:
+    if dir == 'R':
+        b += 1
+    elif dir == 'L':
+        w += 1
+
+print(w, b)
