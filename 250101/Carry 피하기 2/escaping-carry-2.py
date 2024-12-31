c45cee5707a8caceac2e80627888092f892a8b26
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2025-01-01 06:47:35 +0900

    Title: Carry 피하기 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250101/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,28 @@
+n = int(input())
+arr= []
+for i in range(n):
+    inp = int(input())
+    arr.append(inp)
+
+compare = 0
+flag = 0
+mx = 0
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            a = arr[i]
+            b = arr[j]
+            c = arr[k]
+            flag = 0
+            while not(a==0 and b==0 and c==0):
+                compare = a%10+b%10+c%10
+                if compare>=10: 
+                    flag = 1
+                    break
+                a//=10
+                b//=10
+                c//=10
+            if flag == 0:
+                if arr[i]+arr[j]+arr[k]>mx: mx =arr[i]+arr[j]+arr[k]
+            
+print(mx)
