46efcbfa3a2faba80b4a8b6362b1c3d9086cb9bb
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2025-01-01 03:51:45 +0900

    Title: 거울에 레이저 쏘기 2, Time: 5066ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

---
+++ 250101/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,48 @@
+n = int(input())
+arr = []
+for i in range(n):
+    inp = input()
+    arr.append(inp)
+
+k = int(input())
+sx,sy=0,0
+dx=[1,0,-1,0]
+dy=[0,1,0,-1]
+cur = 0
+dir = cur+1
+
+#first block determination
+for i in range(2, k+1):
+    if sx+dx[cur]>=n or sx+dx[cur]<0 or sy+dy[cur]>=n or sy+dy[cur]<0:
+        cur = (cur+1) % 4
+        dir = (cur+1) % 4
+    else: sx,sy=sx+dx[cur],sy+dy[cur]
+
+#raycasting
+x,y=sx,sy
+count = 0
+while x < n and x>=0 and y < n and x>=0:
+    if arr[y][x] == '/':
+        if dir == 0:
+            dir = 3
+        elif dir == 1:
+            dir = 2
+        elif dir == 2:
+            dir = 1
+        else:#dir == 3
+            dir = 0
+    
+    else:
+        if dir == 0:
+            dir = 1
+        elif dir == 1:
+            dir = 0
+        elif dir == 2:
+            dir = 3
+        else:#dir == 3
+            dir = 2
+    
+    count+=1
+    x,y = x+dx[dir],y+dy[dir]
+
+print(count)
