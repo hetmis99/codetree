e7a1e65ae2122f06231731992e4e487687ff4771
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2025-01-02 03:41:58 +0900

    Title: 스승의 은혜 3, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250102/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250102/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -5,17 +5,16 @@ for i in range(n):
     arr.append((p,s))
 
 arr = sorted(arr, key=lambda x: x[0])
-sum = 0
-count = 0
-for i in range(n):
-    sum+=arr[i][0]+arr[i][1]
-    if sum>b: 
-        sum-=arr[i][0]//2
-        if sum>b: break
-        else: 
-            count+=1
-            break
 
-    count+=1
+for i in range(n):
+    sum = 0
+    count = 0
+    tmp = arr.copy()
+    pi,si=tmp[i]
+    tmp[i] = (pi//2,si)
+    for j in range(n):
+        if sum>b: break
+        sum+=tmp[j][0]+tmp[j][1]
+        count+=1
 
 print(count)
