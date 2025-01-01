05dce4bf44384359ae4c8738a84c0480dc2b2dc8
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2025-01-02 06:25:16 +0900

    Title: 전부 겹치는 선분 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 250102/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,10 +1,8 @@
 n = int(input())
 arr = [0 for i in range(101)]
-blocks = []
 
 for i in range(n):
     x1,x2 = map(int,input().split())
-    blocks.append((x1,x2))
     for j in range(x1,x2+1):
         arr[j]+=1
 
