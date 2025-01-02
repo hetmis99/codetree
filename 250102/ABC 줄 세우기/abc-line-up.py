773647b26577be323ef1e9859649d742d33c5f88
hetmis99 <49573217+hetmis99@users.noreply.github.com>
2025-01-02 12:24:57 +0900

    Title: ABC 줄 세우기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/ABC 줄 세우기/abc-line-up.py
+++ 250102/ABC 줄 세우기/abc-line-up.py
@@ -5,7 +5,7 @@ arr = list(map(str, input().split()))
 count=0
 
 for i in range(n-1):
-    for j in range(i, n-1):
+    for j in range(n-i-1):
         if(arr[j]>arr[j+1]):
             arr[j],arr[j+1] = arr[j+1],arr[j]
             count+=1
